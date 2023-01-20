0c2066ecebb5388ef77ddd82cb35e13cdff07b48
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2023-01-21 03:50:45 +0900

    Rename Box and edit export.

--- packages/suggestion4vue/src/index.ts
+++ packages/suggestion4vue/src/index.ts
@@ -1,3 +1,11 @@
-export { default as SearchBox } from './components/SearchBox.vue';
-export { default as SuggestionInput } from './components/SuggestionInput.vue';
-export { default as SuggestionItem } from './components/SuggestionItem.vue';
+import { default as Box } from './components/SuggestionBox.vue';
+import { default as Input } from './components/SuggestionInput.vue';
+import { default as Item } from './components/SuggestionItem.vue';
+
+export { default as Box } from './components/SuggestionBox.vue';
+export { default as Input } from './components/SuggestionInput.vue';
+export { default as Item } from './components/SuggestionItem.vue';
+
+export default {
+  Box, Input, Item
+}
